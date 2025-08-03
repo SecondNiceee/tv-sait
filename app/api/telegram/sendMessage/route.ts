29f499e7bfa4125c05dba3fc8ac843a798372f92
SecondNiceee <col1596321@gmail.com>
2025-08-03 14:08:36 +0300

    Улучшил OPTIONS

--- app/api/telegram/sendMessage/route.ts
+++ app/api/telegram/sendMessage/route.ts
@@ -1,5 +1,12 @@
 import { NextResponse } from 'next/server';
+export async function OPTIONS() {
+  const headers = new Headers();
+  headers.set('Access-Control-Allow-Origin', '*');
+  headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
+  return new Response(null, { status: 200, headers });
+}
 export async function POST(request: Request) {
   try {
     const headers = new Headers();
